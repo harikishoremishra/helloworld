a5679b64f19be4378ebad648f5ae9d816e04ad11
harikishoremishra <122509569+harikishoremishra@users.noreply.github.com>
2023-06-18 08:38:52 +0530

    Add files via upload

---
+++ datatype.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//declare two variable and then assign then value of 3,5 and then add then
+	int a;
+	int b;
+	int c;
+	a=3;
+	b=5;
+	c=a+b;
+	cout<<c;
+	cout<<endl;
+	cout<<"a+b";
+	return 0;
+}
